ffea36258cd496b552009eed0c3398e55877b1a7
charl <charlene.jiang@etu.utc.fr>
2026-05-15 14:04:54 +0200

    fix: improve user API endpoints security and return messages
    - improve the security of all endpoints of user API
    - add UserDTO so the password is not exposed
    - add PasswordService to factorise the code
    - add generic ApiResponse class to customise the endpoints return
      messages

---
+++ src/main/java/fr/utc/sr03/model/UserDTO.java
@@ -0,0 +1,47 @@
+package fr.utc.sr03.model;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+// This class is used to transfer user data without exposing sensitive information like password
+@JsonPropertyOrder({"id", "firstname", "lastname", "mail", "admin", "activated"})
+public class UserDTO {
+    private final Integer id;
+    private final String firstname;
+    private final String lastname;
+    private final String mail;
+    private final Boolean admin;
+    private final Boolean activated;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.firstname = user.getFirstname();
+        this.lastname = user.getLastname();
+        this.mail = user.getMail();
+        this.admin = user.getAdmin();
+        this.activated = user.getActivated();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public Boolean getAdmin() {
+        return admin;
+    }
+
+    public Boolean getActivated() {
+        return activated;
+    }
+}
